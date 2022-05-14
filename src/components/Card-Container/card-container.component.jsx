c5f18ceb1bcb61020ef7d8e00bf884b9176f3a98
SarmadKhalique <sarmadkhalique001@gmail.com>
2022-05-14 19:36:10 +0500

    Changed the API BASE URL

--- src/components/Card-Container/card-container.component.jsx
+++ src/components/Card-Container/card-container.component.jsx
@@ -1,22 +1,22 @@
-import React, { useContext } from "react";
-import { Grid } from "@mui/material";
+import React, {useContext} from "react";
+import {Grid} from "@mui/material";
 import CustomCard from "../Custom-Card/custom-card.component";
-import { PostContext } from "../../context/provider/posts/posts.provider";
-import { ContainerParent } from "./card-container.styles";
+import {PostContext} from "../../context/provider/posts/posts.provider";
+import {ContainerParent} from "./card-container.styles";
 
 const CardContainer = () => {
-  const { posts } = useContext(PostContext);
-  return (
-    <ContainerParent>
-      <Grid container spacing={2} paddingX={2}>
-        {posts.map((post) => (
-          <Grid key={post.id} item md={3}>
-            <CustomCard {...post} />
-          </Grid>
-        ))}
-      </Grid>
-    </ContainerParent>
-  );
+    const {posts} = useContext(PostContext);
+    return (
+        <ContainerParent>
+            <Grid container spacing={2} paddingX={2}>
+                {posts.map((post) => (
+                    <Grid key={post.id} item md={3}>
+                        <CustomCard {...post} />
+                    </Grid>
+                ))}
+            </Grid>
+        </ContainerParent>
+    );
 };
 
 export default CardContainer;
--- src/context/provider/user/user.provider.jsx
+++ src/context/provider/user/user.provider.jsx
@@ -1,95 +1,99 @@
 import axios from "axios";
 import dayjs from "dayjs";
 import jwt_decode from "jwt-decode";
-import React, { createContext, useState } from "react";
+import React, {createContext, useState} from "react";
+import {BASE_URL} from "../../../config/config";
 
 export const UserContext = createContext({
-  checkUserSession: ()=>{},
-  loginUser: ()=>{},
-  logoutUser: ()=>{},
-  userLoggedIn: false,
-  signUpUser: ()=>{},
+    checkUserSession: () => {
+    },
+    loginUser: () => {
+    },
+    logoutUser: () => {
+    },
+    userLoggedIn: false,
+    signUpUser: () => {
+    },
 });
 
-const UserProvider = ({ children }) => {
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
-  const [authTokens, setAuthTokens] = useState(
-    localStorage.getItem("authTokens")
-      ? JSON.parse(localStorage.getItem("authTokens"))
-      : null
-  );
+const UserProvider = ({children}) => {
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [authTokens, setAuthTokens] = useState(
+        localStorage.getItem("authTokens")
+            ? JSON.parse(localStorage.getItem("authTokens"))
+            : null
+    );
 
-  const baseUrl = "http://127.0.0.1:8000";
 
-  let loginUser = async ({ username, password }) => {
-    let response = await axios.post(`${baseUrl}/api/token/`, {
-      username: username,
-      password: password,
-    });
-    if (response.status === 200) {
-      setAuthTokens(response.data);
-      setUserLoggedIn(true);
-      localStorage.setItem("authTokens", JSON.stringify(response.data));
-    } else {
-      alert("An error occoured while logging in the user");
+    let loginUser = async ({username, password}) => {
+        let response = await axios.post(`${BASE_URL}/token/`, {
+            username: username,
+            password: password,
+        });
+        if (response.status === 200) {
+            setAuthTokens(response.data);
+            setUserLoggedIn(true);
+            localStorage.setItem("authTokens", JSON.stringify(response.data));
+        } else {
+            alert("An error occoured while logging in the user");
+        }
+    };
+
+    let logoutUser = () => {
+        setAuthTokens(null);
+        setUserLoggedIn(false);
+        localStorage.removeItem("authTokens");
+    };
+
+    const checkUserSession = async () => {
+        if (!authTokens) {
+            return;
+        }
+        const isTokenExpired =
+            dayjs.unix(jwt_decode(authTokens.access).exp).diff(dayjs()) < 1;
+        if (!isTokenExpired) {
+            setUserLoggedIn(true);
+            return;
+        }
+
+        const response = await axios.post(`${BASE_URL}/api/token/refresh/`, {
+            refresh: authTokens.refresh,
+        });
+        localStorage.setItem("authTokens", JSON.stringify(response.data));
+        setUserLoggedIn(true);
+    };
+
+    const signUpUser = async ({username, email, password, confirmPassword}) => {
+        let password2 = confirmPassword
+        const response = await axios.post(`${BASE_URL}/api/sign-up/`, {
+            username,
+            email,
+            password,
+            password2
+        });
+        if (response.status === 201) {
+            loginUser({username, password})
+        }
+        loginUser(username, password);
+    };
+
+    const values = {
+        checkUserSession,
+        loginUser,
+        logoutUser,
+        userLoggedIn,
+        signUpUser,
+        authTokens
     }
-  };
-
-  let logoutUser = () => {
-    setAuthTokens(null);
-    setUserLoggedIn(false);
-    localStorage.removeItem("authTokens");
-  };
-
-  const checkUserSession = async () => {
-    if (!authTokens) {
-      return;
-    }
-    const isTokenExpired =
-      dayjs.unix(jwt_decode(authTokens.access).exp).diff(dayjs()) < 1;
-      if (!isTokenExpired) {
-      setUserLoggedIn(true);
-      return;
-    }
-
-    const response = await axios.post(`${baseUrl}/api/token/refresh/`, {
-      refresh: authTokens.refresh,
-    });
-    localStorage.setItem("authTokens", JSON.stringify(response.data));
-    setUserLoggedIn(true);
-  };
-
-  const signUpUser = async ({ username, email, password, confirmPassword }) => {
-    let password2 = confirmPassword
-    const response = await axios.post(`${baseUrl}/api/sign-up/`, {
-      username,
-      email,
-      password,
-      password2
-    });
-    if (response.status === 201) {
-      loginUser({username, password})
-    }
-    loginUser(username, password);
-  };
-
-  const values = {
-    checkUserSession,
-    loginUser,
-    logoutUser,
-    userLoggedIn,
-    signUpUser,
-    authTokens
-  }
 
 
-  return (
-    <UserContext.Provider
-      value={values}
-    >
-      {children}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider
+            value={values}
+        >
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 export default UserProvider;
